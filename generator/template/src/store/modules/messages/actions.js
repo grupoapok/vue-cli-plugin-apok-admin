161dc880b73b0f66b82ab7225594140115d88ccc
Manuel Díaz <enrique@grupoapok.com>
2020-03-17 16:33:30 -0400

    agregado comentarios a los modulos y update de la documentacion del jsdoc

--- generator/template/src/store/modules/messages/actions.js
+++ generator/template/src/store/modules/messages/actions.js
@@ -9,14 +9,28 @@ import {
  *
  *@function sendMessage
  * @param context {Object} - Vuex instance context Object
- * @param request
+ * @param request {Any} - mutation payload
  */
 export const sendMessage = (context, request) => {
   context.commit(SEND_MESSAGE, { ...request, id: new Date().getTime() });
 };
 
+/**
+ * @function dismissMessage
+ * @param commit {Function} - destructured context.commit function
+ * @param messageId {Payload} - mutation payload
+ */
 export const dismissMessage = ({ commit }, messageId) => commit(DISMISS_MESSAGE, messageId);
 
+/**
+ * @function resetFields
+ * @param commit {Function} - destructured context.commit function
+ */
 export const resetFields = ({ commit }) => commit(RESET_FIELDS);
 
+/**
+ * @function setFields
+ * @param commit {Function} - destructured context.commit function
+ * @param fields {Payload} - mutations payload
+ */
 export const setFields = ({ commit }, fields) => commit(SET_FIELDS, fields);
