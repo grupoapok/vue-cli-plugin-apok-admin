d9c554279a932041b96dc5567c62bd92e202057c
Manuel Díaz <enrique@grupoapok.com>
2020-04-20 19:59:14 -0400

    agregada las pruebas del componente Login, correccion en NotFoundPage.component.spec.js

--- generator/template/src/tests/unit/Login.component.spec.js
+++ generator/template/src/tests/unit/Login.component.spec.js
@@ -1,17 +1,32 @@
 import {createLocalVue, shallowMount} from "@vue/test-utils";
 import Login from '@/views/Login';
-import Vuex from 'vuex'
 
 const localVue = createLocalVue();
-localVue.use(Vuex);
 
+describe('Login component', () => {
+    const doLoginMock = jest.fn();
+    const wrapper = shallowMount(Login, {
+        localVue,
+        data(){
+            return {
+                user: {
+                    username: 'johndoe@example.com',
+                    password: 'password',
+                }
+            }
+        },
+        computed: {
+            loading: () => false,
+            fields: () => 'test',
+        },
+        methods: {
+            doLogin: doLoginMock,
+        },
+    });
 
-/*Login component tests*/
-describe('Login.vue', () => {
-
-    it('renders correctly ', () => {
-        const shallowWrapperLogin = shallowMount(Login);
-        console.log(shallowWrapperLogin.html());
-
+    it('triggers "doSubmit" method with the right payload when button is clicked ', async () => {
+        wrapper.find('admin-form').trigger('submit');
+        await wrapper.vm.$nextTick();
+        expect(doLoginMock).toBeCalledWith({username: 'johndoe@example.com', password: 'password'})
     });
 });
